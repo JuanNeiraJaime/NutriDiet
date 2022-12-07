25540a69b54f66c736c9acd0335b3fb99e2f9176
JuanNeiraJaime <l17100264@nlared.tecnm.mx>
2022-12-06 22:05:49 -0600

    otromerge

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,7 @@
-export * from './ping.controller';
 export * from './alimento.controller';
-export * from './paciente.controller';
-export * from './user.controller';
-<<<<<<< HEAD
-export * from './progresos.controller';
-=======
 export * from './cita.controller';
->>>>>>> a76a2e6f236271bd510fef4a65ea41b50d210987
+export * from './paciente.controller';
+export * from './ping.controller';
+export * from './progresos.controller';
+export * from './user.controller';
+
